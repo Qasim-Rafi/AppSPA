664688805846bb5cdf20005088b6f27dbeb85756
Qasim-Rafi <68726598+Qasim-Rafi@users.noreply.github.com>
2020-08-19 17:13:18 +0500

    first commit

---
+++ CoreWebApi/CoreWebApi/Data/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using CoreWebApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreWebApi.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+
+
+        public DbSet<Value> Values { get; set; }
+
+
+
+    }
+
+}
---
+++ CoreWebApi/CoreWebApi/Models/Value.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreWebApi.Models
+{
+    public class Value
+    {
+        public int Id { get; set; }
+
+
+  
+        [Column(TypeName = "varchar(200)")]
+        [Required]
+        public string  Name { get; set; }
+
+     
+
+       
+
+    }
+}
