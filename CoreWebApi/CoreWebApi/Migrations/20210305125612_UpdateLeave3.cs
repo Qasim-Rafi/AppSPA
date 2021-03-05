7d8f5bc340448326e88e1f570dc690d831632eb8
Ijaz-Tufail <71372593+Ijaz-Tufail@users.noreply.github.com>
2021-03-05 17:56:51 +0500

    update leave model

---
+++ CoreWebApi/CoreWebApi/Migrations/20210305125612_UpdateLeave3.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CoreWebApi.Migrations
+{
+    public partial class UpdateLeave3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ApproveById",
+                table: "Leaves",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ApproveComment",
+                table: "Leaves",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ApproveDateTime",
+                table: "Leaves",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ApproveById",
+                table: "Leaves");
+
+            migrationBuilder.DropColumn(
+                name: "ApproveComment",
+                table: "Leaves");
+
+            migrationBuilder.DropColumn(
+                name: "ApproveDateTime",
+                table: "Leaves");
+        }
+    }
+}
